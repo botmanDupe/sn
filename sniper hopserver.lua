3548f4179314f124bcec30fd11ac5d847dfd9160
botmanDupe <154759264+botmanDupe@users.noreply.github.com>
2024-01-01 17:44:38 -0600

    Update sniper hopserver.lua

--- sniper hopserver.lua
+++ sniper hopserver.lua
@@ -1,83 +1,32 @@
 repeat wait() until game:IsLoaded()
 
--- SETTINGS (adjust as needed)
-local MINIMUM_PLAYERS = 10
-local pingThreshold = 310
-local gameId = 15502339080  -- Replace with the desired game ID
-
--- Services
-local HttpService = game:GetService("HttpService")
-local TeleportService = game:GetService("TeleportService")
-local Players = game:GetService("Players")
-local StatsService = game:GetService("Stats")
-
--- Variables
-local serverStats = StatsService.Network.ServerStatsItem
-local dataPing = serverStats["Data Ping"]:GetValueString()
-local pingValue = tonumber(dataPing:match("(%d+)"))
-local PlaceId = game.PlaceId
-local fileName = string.format("%s_servers.json", tostring(PlaceId))
-local ServerHopData = {
-    CheckedServers = {},
-    LastTimeHop = nil,
-    CreatedAt = os.time()
-}
-
--- Load previous server data
-if isfile(fileName) then
-    local fileContent = readfile(fileName)
-    ServerHopData = HttpService:JSONDecode(fileContent)
-end
-
--- Function to find and join a suitable server
-local function jumpToServer()
-    local function GetServerList(cursor)
-        cursor = cursor and "&cursor=" .. cursor or ""
-        local API_URL = string.format('https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Desc&limit=100', tostring(PlaceId))
-        return HttpService:JSONDecode(game:HttpGet(API_URL .. cursor))
-    end
-
-    local currentPageCursor = nil
-    while true do
-        local serverList = GetServerList(currentPageCursor)
-        currentPageCursor = serverList.nextPageCursor
-
-        for _, server in ipairs(serverList.data) do
-            if server.playing and tonumber(server.playing) >= MINIMUM_PLAYERS and tonumber(server.playing) < Players.MaxPlayers and not table.find(ServerHopData.CheckedServers, tostring(server.id)) and server.id ~= game.JobId and server.playing >= 1 then
-                -- Update server hop data
-                ServerHopData.LastTimeHop = os.time()
-                table.insert(ServerHopData.CheckedServers, server.id)
-                writefile(fileName, HttpService:JSONEncode(ServerHopData))
-
-                -- Teleport to the server
-                TeleportService:TeleportToPlaceInstance(PlaceId, server.id, game.Players.LocalPlayer)
-                wait(0.5)
-
-                -- Check if trade server is full
-                if server.playing >= Players.MaxPlayers then
-                    -- Redirect back to the original server
-                    TeleportService:TeleportToPlaceInstance(PlaceId, game.JobId, game.Players.LocalPlayer)
-                    return
-                end
-
-                return  -- Exit the loop after successful hop
+local function jumpToServer() 
+local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
+local req = request({ Url = string.format(sfUrl, 15502339080, "Desc", 100) }) 
+local body = game:GetService("HttpService"):JSONDecode(req.Body) 
+local deep = math.random(1, 3)
+if deep > 1 then 
+    for i = 1, deep, 1 do 
+        req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 15502339080, "Desc", 100) }) 
+        body = game:GetService("HttpService"):JSONDecode(req.Body) 
+        task.wait(0.1)
+    end 
+end 
+local servers = {} 
+if body and body.data then 
+    for i, v in next, body.data do 
+            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+                table.insert(servers, 1, v.id)
             end
         end
-
-        if not currentPageCursor then break else wait(0.25) end
     end
-end
--- Function to check ping and trigger server hopping if necessary
-local function CheckPingAndHop()
-    if pingValue >= pingThreshold then
-        print("Ping Is Higher Than 310, Server Hopping...")
-        jumpToServer()
-    else
-        print("Ping is sufficient. No Server Hop Needed")
+    local randomCount = #servers
+    if not randomCount then
+        randomCount = 2
     end
+    game:GetService("TeleportService"):TeleportToPlaceInstance(15502339080, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer) 
 end
 
--- Main loop
 while wait(1) do
-    CheckPingAndHop()  -- Check ping frequently
+    jumpToServer()
 end
