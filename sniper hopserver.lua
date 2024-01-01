393b238e06eb91c0649110ebd0d51dfce4baa6bd
botmanDupe <154759264+botmanDupe@users.noreply.github.com>
2024-01-01 16:05:51 -0600

    Update sniper hopserver.lua

--- sniper hopserver.lua
+++ sniper hopserver.lua
@@ -43,7 +43,7 @@ local function jumpToServer()
         currentPageCursor = serverList.nextPageCursor
 
         for _, server in ipairs(serverList.data) do
-            if server.playing and tonumber(server.playing) >= MINIMUM_PLAYERS and tonumber(server.playing) < Players.MaxPlayers and not table.find(ServerHopData.CheckedServers, tostring(server.id)) and server.id ~= game.JobId then
+            if server.playing and tonumber(server.playing) >= MINIMUM_PLAYERS and tonumber(server.playing) < Players.MaxPlayers and not table.find(ServerHopData.CheckedServers, tostring(server.id)) and server.id ~= game.JobId and server.playing >= 1 then
                 -- Update server hop data
                 ServerHopData.LastTimeHop = os.time()
                 table.insert(ServerHopData.CheckedServers, server.id)
@@ -52,6 +52,14 @@ local function jumpToServer()
                 -- Teleport to the server
                 TeleportService:TeleportToPlaceInstance(PlaceId, server.id, game.Players.LocalPlayer)
                 wait(0.5)
+
+                -- Check if trade server is full
+                if server.playing >= Players.MaxPlayers then
+                    -- Redirect back to the original server
+                    TeleportService:TeleportToPlaceInstance(PlaceId, game.JobId, game.Players.LocalPlayer)
+                    return
+                end
+
                 return  -- Exit the loop after successful hop
             end
         end
@@ -59,7 +67,6 @@ local function jumpToServer()
         if not currentPageCursor then break else wait(0.25) end
     end
 end
-
 -- Function to check ping and trigger server hopping if necessary
 local function CheckPingAndHop()
     if pingValue >= pingThreshold then
